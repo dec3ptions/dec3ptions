b3e78e0d43187416452469db59a18fcda5732676
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2023-04-07 18:32:11 +0100

    coems

--- other2.js
+++ other2.js
@@ -1,23 +1,3 @@
 let dec3ptions_blueprint = await fetch('https://raw.githubusercontent.com/dec3ptions/dec3ptions/hacks/dec3ptions/dec3ptions_blueprint.js') 
   .then(response => response.text());
   console.log(dec3ptions_blueprint);
-
-
-const url = 'https://raw.githubusercontent.com/dec3ptions/dec3ptions/hacks/dec3ptions/dec3ptions_blueprint.js';
-
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text();
-  })
-  .then(text => {
-    const script = new Function(`module.exports = ${text}`);
-    const data = script();
-    const dec3ptions_blueprint = data;
-    console.log(dec3ptions_blueprint);
-  })
-  .catch(error => {
-    console.error('Error fetching file:', error);
-  });
